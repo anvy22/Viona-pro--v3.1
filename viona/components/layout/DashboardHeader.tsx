046e7dcbb11eb239b25c2bcdab0137d47ed43d5d
Anvin George <anvingeorge22@gmail.com>
2026-03-05 10:20:07 +0530

    made  some ui fixes

--- viona/components/layout/DashboardHeader.tsx
+++ viona/components/layout/DashboardHeader.tsx
@@ -30,6 +30,11 @@ function shouldShowOrgSelector(pathname: string): boolean {
 
 export const DashboardHeader = React.memo(function DashboardHeader() {
   const pathname = usePathname();
+  
+  // Check if we are in the workflow editor (e.g., /workflows/[workflowId])
+  const isWorkflowEditor = pathname.match(/^\/workflows\/[^/]+$/);
+  if (isWorkflowEditor) return null;
+
   const { selectedOrgId, orgs, setSelectedOrgId } = useOrgStore();
   const showOrgSelector = shouldShowOrgSelector(pathname);
 
